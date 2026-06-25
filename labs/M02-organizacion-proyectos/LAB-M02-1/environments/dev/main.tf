52f1808e7a9065cc2897de6de2a5164d43079549
jorpelo <jperez.lopez@knowmadmood.com>
2026-06-25 16:47:44 +0000

    feat(dev): añade common_tags con Owner

---
+++ labs/M02-organizacion-proyectos/LAB-M02-1/environments/dev/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+locals {
+  name_prefix = "${var.project}-${var.environment}"
+  common_tags = {
+    Project     = var.project
+    Environment = var.environment
+    Owner       = "equipo-dev"
+  }
+}
+
+output "name_prefix" {
+  value = local.name_prefix
+}
